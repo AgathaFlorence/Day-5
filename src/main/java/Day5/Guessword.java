114a45d42b453f7b4583683d12b0582621aa7857
AgathaFlorence <agathaflorencetjhia@outlook.com>
2024-09-06 16:00:45 +0700

    Feat select random word

--- src/main/java/Day5/Guessword.java
+++ src/main/java/Day5/Guessword.java
@@ -1,14 +1,31 @@
 package Day5;
 
+import java.util.Random;
+
+
 public class Guessword {
     public static void main (String[] args) {
-        String wordtoguess = "method";
+
+        String wordtoguess = selectRandomWord();
         int n = wordtoguess.length();
 
         hidewords(n);
 
 
     }
+
+    public static String selectRandomWord(){
+        String [] wordlist = {"dog","bird","fly","cat","bear"};
+        Random randomword = new Random();
+        int randomInd = randomword.nextInt(wordlist.length);
+
+        return wordlist[randomInd];
+
+    }
+
+
+
+
     public static void hidewords(int a) {
         for (int i = 1; i<=a;i++){
         System.out.print("_");}
